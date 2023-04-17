ce0bd47aac7f4218b714b7db56b7a151c41ddd5c
Ederick Wong <ederickwong98@gmail.com>
2023-04-17 09:28:55 -0700

    Backend

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import threading
+from __init__ import app, db
+# from api.athlete import athlete_api 
 
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
 
 # import "packages" from "this" project
-from __init__ import app,db  # Definitions initialization
-from model.jokes import initJokes
-from model.users import initUsers
-from model.players import initPlayers
+from __init__ import app  # Definitions initialization
+# from model.jokes import initJokes
+# from model.users import initUsers
+from model.sport import initSports
 
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
-
-
+# from api.covid import covid_api # Blueprint import api definition
+# from api.joke import joke_api # Blueprint import api definition
+# from api.user import user_api # Blueprint import api definition
+from api.sport import sport_api
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(player_api)
+# app.register_blueprint(joke_api) # register api routes
+# app.register_blueprint(covid_api) # register api routes
+# app.register_blueprint(user_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(sport_api)
+# app.register_blueprint(signup_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -38,18 +39,29 @@ def index():
 
 @app.route('/stub/')  # connects /stub/ URL to stub() function
 def stub():
-    return render_template("stub.html")
+    return render_template("stub.html")   
 
 @app.before_first_request
-def activate_job():  # activate these items 
+def activate_job():
     db.init_app(app)
-    initJokes()
-    initUsers()
-    initPlayers()
+    # initJokes()
+    # initUsers()
+    # initAthletes()
+    # # initSignups()
+    # initEtrackUsers()
+    # initPost()
+    initSports()
+    
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
+  return response
 
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    from flask_cors import CORS
-    cors = CORS(app)
+    # from flask_cors import CORS
+    # cors = CORS(app)
     app.run(debug=True, host="0.0.0.0", port="8086")
